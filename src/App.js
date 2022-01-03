816a883c99c3e5d5e6f8af4f9d57f1e767d1b07a
Derek Reid <itsjustdel@gmail.com>
2022-01-03 20:19:24 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';   
+import './App.css';         
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {             
+  // we can write JS here :)  
+  const age = 25;
+  const name = "Billy";
+
+  return (          
+        //JSX - Javascript XML          
+        <div>
+          <h1 className="title">Hello World!</h1>
+          <p> This is a paragraph of text</p>
+          <p>My name is not {name} and I'm not {age} years old</p>
+        </div>     
+  );                         
 }
 
-export default App;
+export default App;           
